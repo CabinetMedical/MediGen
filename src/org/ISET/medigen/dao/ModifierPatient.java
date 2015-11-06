9055b65a31e999993a561d1c32eb3f94e46e24ce
SYSTEM <SYSTEM@compac-PC>
2015-11-06 22:18:51 +0100

    modifier patient

--- src/org/ISET/medigen/dao/ModifierPatient.java
+++ src/org/ISET/medigen/dao/ModifierPatient.java
@@ -10,7 +10,7 @@ public class ModifierPatient extends javax.swing.JFrame {
         
     }
 
-
+//////////////////////
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
